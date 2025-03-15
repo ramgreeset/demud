7733fa3ecbc7531571a10b8fcdf5c45a53f5ff3a
Roman Artemyev <artemyev.ram@gmail.com>
2025-03-16 00:12:31 +0300

    Добавил блок faq

--- wp-content/themes/demud/woocommerce/archive-product.php
+++ wp-content/themes/demud/woocommerce/archive-product.php
@@ -101,6 +101,123 @@ do_action('woocommerce_before_main_content');
         </section>
     </div>
 </section>
+<section class="section faq">
+    <div class="container">
+        <div class="section__header">
+            <h2 class="section__title">Вопросы и ответы</h2>
+        </div>
+        <div class="accordion js-accordion">
+            <div class="accordion__item js-accordion-item">
+                <div class="accordion__header js-accordion-header">
+                    <span class="accordion__title title-h4">Как ухаживать за изделием?</span>
+                    <svg class="accordion__icon icon">
+                        <use href="<?php echo get_template_directory_uri(); ?>/assets/sprite.svg#caret-down"></use>
+                    </svg>
+                </div>
+                <div class="accordion__body js-accordion-body">
+                    <div class="accordion__content content">
+                        <ol>
+                            <li>
+                                Изделия можно протирать тряпочкой. При необходимости помыть под струей холодной
+                                воды.
+                                Мытье в горячей воде и контакт с щелочами не рекомендуется , возможно повреждение
+                                поверхностного
+                                слоя.
+                            </li>
+                            <li>
+                                Предметы не предназначены для употребления пищи людьми и животными.
+                            </li>
+                            <li>
+                                Избегать длительный контакт с водой.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion__item js-accordion-item">
+                <div class="accordion__header js-accordion-header">
+                    <span class="accordion__title title-h4">Как оформить заказ?</span>
+                    <svg class="accordion__icon icon">
+                        <use href="<?php echo get_template_directory_uri(); ?>/assets/sprite.svg#caret-down"></use>
+                    </svg>
+                </div>
+                <div class="accordion__body js-accordion-body">
+                    <div class="accordion__content content">
+                        <ol>
+                            <li>
+                                Изделия можно протирать тряпочкой. При необходимости помыть под струей холодной
+                                воды.
+                                Мытье в горячей воде и контакт с щелочами не рекомендуется , возможно повреждение
+                                поверхностного
+                                слоя.
+                            </li>
+                            <li>
+                                Предметы не предназначены для употребления пищи людьми и животными.
+                            </li>
+                            <li>
+                                Избегать длительный контакт с водой.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion__item js-accordion-item">
+                <div class="accordion__header js-accordion-header">
+                    <span class="accordion__title title-h4">Как оплатить заказ?</span>
+                    <svg class="accordion__icon icon">
+                        <use href="<?php echo get_template_directory_uri(); ?>/assets/sprite.svg#caret-down"></use>
+                    </svg>
+                </div>
+                <div class="accordion__body js-accordion-body">
+                    <div class="accordion__content content">
+                        <ol>
+                            <li>
+                                Изделия можно протирать тряпочкой. При необходимости помыть под струей холодной
+                                воды.
+                                Мытье в горячей воде и контакт с щелочами не рекомендуется , возможно повреждение
+                                поверхностного
+                                слоя.
+                            </li>
+                            <li>
+                                Предметы не предназначены для употребления пищи людьми и животными.
+                            </li>
+                            <li>
+                                Избегать длительный контакт с водой.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion__item js-accordion-item">
+                <div class="accordion__header js-accordion-header">
+                    <span class="accordion__title title-h4">Как оформить доставку?</span>
+                    <svg class="accordion__icon icon">
+                        <use href="<?php echo get_template_directory_uri(); ?>/assets/sprite.svg#caret-down"></use>
+                    </svg>
+                </div>
+                <div class="accordion__body js-accordion-body">
+                    <div class="accordion__content content">
+                        <ol>
+                            <li>
+                                Изделия можно протирать тряпочкой. При необходимости помыть под струей холодной
+                                воды.
+                                Мытье в горячей воде и контакт с щелочами не рекомендуется , возможно повреждение
+                                поверхностного
+                                слоя.
+                            </li>
+                            <li>
+                                Предметы не предназначены для употребления пищи людьми и животными.
+                            </li>
+                            <li>
+                                Избегать длительный контакт с водой.
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 
 /**
